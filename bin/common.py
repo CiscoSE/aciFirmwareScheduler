ca62772bfc09df31fa052ed50801807b4bcf192c
oldguardmd <oldguard@msn.com>
2022-03-10 17:47:47 -0500

    Working Framework - No functionality

---
+++ bin/common.py
@@ -0,0 +1,89 @@
+import requests
+import time
+import json
+import re
+
+#Required for self signed certificate handling.
+from urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+class loggingFunctions:
+    def __init__(self):
+        return
+
+    def writeEvent(self, msg, msgType='INFO'):
+        self.writeScreen(msg, msgType)
+        return
+
+    def writeLog(self):
+        #If we add output to file later, this may get used. 
+        return
+
+    def writeScreen(self, msg, msgType='INFO'):
+        if msgType == 'INFO':
+            print(f"[ {textColors.INFO}INFO{textColors.noColor} ] {msg}")
+        elif msgType == 'WARN':
+            print(f"[ {textColors.WARN}WARN{textColors.noColor} ] {msg}")
+        elif msgType == "FAIL":
+            print(f"[ {textColors.FAIL}FAIL{textColors.noColor} ] {msg}")
+            exit()
+        else:
+            print(f"[ {textColors.FAIL}UNKNOWN{textColors.noColor} ] {msg}")
+            print(f"This is a developer bug. Script will exit")
+            exit()
+        return
+
+class textColors:
+        noColor = '\x1b[0m'
+        INFO = '\033[32m'
+        WARN = '\033[33m'
+        FAIL = '\033[31m'
+
+class timeFunctions:
+    def __init__(self):
+        return
+
+    def getCurrentTime(self):
+        return time.strftime("%Y%m%d%H%M%S")
+
+class urlFunctions:
+    def __init__(self, args):
+        self.debug = args.debug
+        self.apicUser = args.apicUser
+        self.password = args.password
+        self.apicName = args.apicName
+        return
+
+    def getData(self, url, data='', headers={"Content-Type": "Application/json"},requestType='post', cookie='' ):
+        if self.debug == True:
+            loggingFunctions().writeEvent(msg=f"URL:\t\t{url}", msgType='INFO')
+            loggingFunctions().writeEvent(msg=f"Data:\t\t{data}".replace(self.password,"REMOVED_PASSWORD"),msgType='INFO')
+            loggingFunctions().writeEvent(msg=f"Headers:\t{headers}",msgType='INFO')
+            loggingFunctions().writeEvent(msg=f"request Type:\t{requestType}", msgType='INFO')
+        if requestType == 'post':
+            try:
+                request = requests.post(url, data=data, headers=headers, timeout=10, cookies=cookie, verify=False)
+            except:
+               loggingFunctions().writeEvent(msg=f"Unable to access {self.apicName}",msgType='FAIL')
+        elif requestType == 'get':
+            try:
+                request = requests.get(url, headers=headers, verify=False, timeout=10, cookies=cookie)
+            except:
+                loggingFunctions().writeEvent(msg=f"Unable to access {self.apicName}",msgType='FAIL') 
+        if re.match("20[0-9]", f"{request.status_code}"):
+            if self.debug == True:
+                loggingFunctions().writeEvent(msg="Request was successful", msgType='INFO')
+            return request.text
+        else:
+            loggingFunctions().writeEvent(msg='Failed to access APIC API', msgType='WARN')
+            loggingFunctions().writeEvent(msg=f'Reason: {request.reason}', msgType='WARN')
+            if self.debug == True:
+                loggingFunctions().writeEvent(msg=request.text, msgType='FAIL')
+            exit()
+
+    def getCookie(self):
+        name_pwd = {'aaaUser':{'attributes': {'name': self.apicUser, 'pwd': f"{self.password}"}}}
+        json_credentials = json.dumps(name_pwd)
+        logonRequest = self.getData(url=f"https://{self.apicName}/api/aaaLogin.json", data=json_credentials, headers={"Content-Type": "application/json"})
+        logonRequestAttributes = json.loads(logonRequest)['imdata'][0]['aaaLogin']['attributes']
+        return logonRequestAttributes['token']
