5aaf64cd47f8791032230cdb7ff2bfca52f1f1bf
oldguardmd <oldguard@msn.com>
2022-03-15 15:13:34 -0400

    Cosmetic - Changed Code Comments

--- bin/common.py
+++ bin/common.py
@@ -14,7 +14,7 @@ IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 or implied.'''
 
 import requests
-import time, json, re, os, datetime
+import time, json, re
 
 #Required for self signed certificate handling.
 from urllib3.exceptions import InsecureRequestWarning
@@ -26,6 +26,7 @@ class loggingFunctions:
         return
 
     def writeEvent(self, msg, msgType='INFO'):
+        # We call this so that if we want to write a log file in the future, we have that option.
         self.writeScreen(msg, msgType)
         return
 
@@ -53,9 +54,9 @@ class loggingFunctions:
 
 class textColors:
         noColor = '\x1b[0m'
-        INFO = '\033[32m'
-        WARN = '\033[33m'
-        FAIL = '\033[31m'
+        INFO = '\033[32m'   # Green
+        WARN = '\033[33m'   # Yellow
+        FAIL = '\033[31m'   # Red
 
 class timeFunctions:
     def __init__(self):
@@ -84,24 +85,32 @@ class urlFunctions:
             loggingFunctions(self.silent).writeEvent(msg=f"Headers:\t{headers}",msgType='INFO')
             loggingFunctions(self.silent).writeEvent(msg=f"request Type:\t{requestType}", msgType='INFO')
         if requestType == 'post':
+            # This is for making changes. 
             if self.debug >= 2:
                 loggingFunctions(self.silent).writeEvent(msg=f"Starting post call to APIC", msgType='INFO')
             try:
+                # Our attempt post changes to the API
                 request = requests.post(url, data=data, headers=headers, timeout=10, cookies=cookie, verify=False)
             except:
-               loggingFunctions(self.silent).writeEvent(msg=f"Unable to access {self.apicName}",msgType='FAIL')
+                # This will terminate the script because our call was not successful.
+                loggingFunctions(self.silent).writeEvent(msg=f"Unable to access {self.apicName}",msgType='FAIL')
         elif requestType == 'get':
+            # Used when we only want to read data from the API
             if self.debug >= 2:
                 loggingFunctions(self.silent).writeEvent(msg=f"Starting get call to APIC", msgType='INFO')
             try:
+                # Our attempt to read data from the API
                 request = requests.get(url, headers=headers, verify=False, timeout=10, cookies=cookie)
             except:
+                # This will terminate the script because our call was not successful.
                 loggingFunctions(self.silent).writeEvent(msg=f"Unable to access {self.apicName}",msgType='FAIL') 
         if re.match("20[0-9]", f"{request.status_code}"):
+            # We are looking for a 200 result that tells us were were successful in accessing the API
             if self.debug >= 1:
                 loggingFunctions(self.silent).writeEvent(msg="Request was successful", msgType='INFO')
             return request.text
         else:
+            # Output if we received something other than a 20x return code. 
             loggingFunctions(self.silent).writeEvent(msg='Failed to access APIC API', msgType='WARN')
             loggingFunctions(self.silent).writeEvent(msg=f'Reason: {request.reason}', msgType='WARN')
             if self.debug >= 1:
@@ -109,17 +118,23 @@ class urlFunctions:
             exit()
 
     def getCookie(self):
+        # Handling Domain support.
         if self.domain != '':
+            # We have a domain name, so we are formating the request differently.
             name_pwd = {'aaaUser':{'attributes': {'name': f'apic:{self.domain}\\{self.apicUser}', 'pwd': f"{self.password}"}}}
         else:
+            #If there is no domain, we format the JSON without the domain structure. 
             name_pwd = {'aaaUser':{'attributes': {'name': self.apicUser, 'pwd': f"{self.password}"}}}
+        # Making sure we have the right JSON format. 
         json_credentials = json.dumps(name_pwd)
+        # Calling getData to access the API.
         logonRequest = self.getData(url=f"https://{self.apicName}/api/aaaLogin.json", data=json_credentials, headers={"Content-Type": "application/json"})
         if self.debug > 1:
             loggingFunctions(self.silent).writeEvent(msg=f'Logon Request Response:\n{logonRequest}')
         logonRequestAttributes = json.loads(logonRequest)['imdata'][0]['aaaLogin']['attributes']
         if self.debug > 2:
             loggingFunctions(self.silent).writeEvent(msg=f'Logon Request Attributes\n{logonRequestAttributes}', msgType='INFO')
+        # We only return the token, so it is important to reformat that into a cookie later if we want to use it.
         return {'APIC-Cookie': logonRequestAttributes['token']}
 
 class inputSupport:
